fb85882fc87113f98374e46860deb530ae9e5796
mariechanta <52567733+mariechanta@users.noreply.github.com>
2024-02-15 16:25:13 +0100

    Last Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import { useState } from "react";
-function ListContent(){
-    const items= ["Elev Profil", "Kalender", "Kontaktlista", "Attendens", "Documentation", "Elev Assecement", "Elev lifeEvent", "BlogElevs", "clothers", "Education", "Plan"]
-    let selectedIndex= -1;
-    //Hook
-    const [selectedIndex, setSelectedIndex]= useSate(-1);
-    // variable (selectedIndex)
-    // updater function(setSelectedIndex)
-    return(
+import { useState } from 'react'
+import React from 'react'
+
+function ListContent() {
+    const items = [
+        'Elev Profil',
+        'Kalender',
+        'Kontaktlista',
+        'Attendens',
+        'Documentation',
+        'Elev Assecement',
+        'Elev lifeEvent',
+        'BlogElevs',
+        'clothers',
+        'Education',
+        'Plan'
+    ];
+
+    return (
         <>
-        <h1> Welcome to elev Attendens (EA)!</h1>
-        <p> List of content</p>
-        <ul className ="list-content">
-            {items.map ((item)=><li key= {item}>{item}</li>)}
-        </ul>
+            <h1> Welcome to elev Attendens (EA)!</h1>
+            <p> List of content</p>
+            <ul className="list-content">
+                {items.map((item) => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
         </>
-    )
+    );
 }
-export default ListContent;
+export default ListContent
